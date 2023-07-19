aaf7abbef86e53105d0178eeef29efde372f8f2c
DanGraziano <dangraziano@outlook.com>
2023-07-19 16:21:28 -0400

    Add routing

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,9 +1,12 @@
+import {Link} from "react-router-dom";
 function Tuiter() {
-    return(
-       <div>
-          <h1>Tuiter</h1>
-       </div>
-    );
- }
- export default Tuiter
- 
+ return(
+  <div>
+   <Link to="/labs/a3">A3</Link> |
+   <Link to="/hello">Hello</Link> |
+   <Link to="/tuiter">Tuiter</Link>
+   <h1>Tuiter</h1>
+  </div>
+ )
+}
+export default Tuiter
