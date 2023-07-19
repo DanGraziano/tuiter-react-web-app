17492ee3d1601f8ea0adde82a9529732a14dae7d
DanGraziano <dangraziano@outlook.com>
2023-07-19 16:32:21 -0400

    Add nav file and code

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,12 +1,12 @@
-import {Link} from "react-router-dom";
 import Assignment3 from "./a3";
+import Nav from "../nav";
+
+
 function Labs() {
  return(
-  <div>
-   <Link to="/labs/a3">A3</Link> |
-   <Link to="/hello">Hello</Link> |
-   <Link to="/tuiter">Tuiter</Link>
-   <Assignment3/>
+   <div>
+      <Nav/>
+      <Assignment3/>
   </div>
  )
 }
