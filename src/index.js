bda3e2c63754220df7df0a0ffb8d143e642a266e
DanGraziano <dangraziano@outlook.com>
2023-07-19 09:43:32 -0400

    Initial commit on A3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; // Will remove
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import 'bootstrap/dist/css/bootstrap.min.css'; // Will addd
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
