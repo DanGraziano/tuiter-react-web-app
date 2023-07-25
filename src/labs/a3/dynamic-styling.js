a6c0ac262a5657e8775d96d6f7841332822d7a96
DanGraziano <dangraziano@outlook.com>
2023-07-25 13:51:30 -0400

    Add dynamic styling

---
+++ src/labs/a3/dynamic-styling.js
@@ -0,0 +1,13 @@
+import Classes from "./classes"
+import Styles from "./styles"
+
+function DynamicStyling () {
+    return (
+        <>
+            <h1>Dynamic Styling</h1>
+            <Styles/>
+            <Classes/>
+        </>
+    )
+}
+export default DynamicStyling
--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -1,8 +1,7 @@
 import JavaScript from "./java-script";
-import Classes from "./classes";
-import Styles from "./styles";
 import ConditionalOutput from "./conditional-output";
 import TodoList from "./todo/todo-list"
+import DynamicStyling from "./dynamic-styling";
 
 function Assignment3() {
   
@@ -11,8 +10,7 @@ function Assignment3() {
       <h1>Assignment 3</h1>
       <TodoList/>
       <ConditionalOutput/>
-      <Styles/>
-      <Classes/>
+      <DynamicStyling/>
       <JavaScript/>
     </div>
   );
