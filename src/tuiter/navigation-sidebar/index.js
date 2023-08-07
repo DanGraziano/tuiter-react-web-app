f9d2abe804ea4657a3d933777301f984603e2558
DanGraziano <dangraziano@outlook.com>
2023-08-07 16:38:12 -0400

    Add conditional rendering to nav bar for login/register and profile

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,40 +1,70 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faHouse, faHashtag, faBell, faEnvelope, faBookmark, faList, faUser, faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import {faHouse,faHashtag,faBell,faEnvelope,faBookmark,faList,faUser,faEllipsis,} from "@fortawesome/free-solid-svg-icons";
 
 const NavigationSidebar = () => {
   const { currentUser } = useSelector((state) => state.user);
-    const { pathname } = useLocation();
-    const [ignore, tuiter, active] = pathname.split("/");
-    const links = ["home", "explore", "notifications", "messages", "bookmarks", "lists", "profile",  "more"];
-  
-    const icons = {
-        home: faHouse,
-        explore: faHashtag,
-        notifications: faBell,
-        messages: faEnvelope,
-        bookmarks: faBookmark,
-        lists: faList,
-        profile: faUser,
-        more: faEllipsis,
-    };
-  
-    return (
-      <div className="list-group">
-        {links.map((link) => (
-          <Link to={`/tuiter/${link}`} className={`text-capitalize list-group-item list-group-item-action d-flex align-items-center ${active === link ? "active" : ""}`} key={link}>
-            <FontAwesomeIcon icon={icons[link]} />
-            <div className="d-none d-xl-block d-xxl-block ps-2">{link}</div>
-          </Link>
-        ))}
-        {!currentUser && <Link className="list-group-item" to="/tuiter/login">   Login   </Link>}
-        {!currentUser && <Link className="list-group-item" to="/tuiter/register">Register</Link>}
-        { currentUser && <Link className="list-group-item" to="/tuiter/profile"> Profile </Link>}
-      </div>
-    );
+  const { pathname } = useLocation();
+  const [ignore, tuiter, active] = pathname.split("/");
+  const links = ["home", "explore", "notifications", "messages", "bookmarks", "lists"];
+
+  const icons = {
+    home: faHouse,
+    explore: faHashtag,
+    notifications: faBell,
+    messages: faEnvelope,
+    bookmarks: faBookmark,
+    lists: faList,
+    profile: faUser,
+    more: faEllipsis,
   };
-  
-  export default NavigationSidebar;
-  
+
+  return (
+    <div className="list-group">
+      {links.map((link) => (
+        <NavLink
+          to={`/tuiter/${link}`}
+          className={`text-capitalize list-group-item list-group-item-action d-flex align-items-center`}
+          activeClassName="active"
+          key={link}
+        >
+          <FontAwesomeIcon icon={icons[link]} className="me-2" />
+          {link}
+        </NavLink>
+      ))}
+      {!currentUser ? (
+        <>
+          <NavLink
+            to="/tuiter/login"
+            className="list-group-item list-group-item-action"
+            activeClassName="active" 
+          >
+            <FontAwesomeIcon icon={faUser} className="me-2" />
+            Login
+          </NavLink>
+          <NavLink
+            to="/tuiter/register"
+            className="list-group-item list-group-item-action"
+            activeClassName="active"
+          >
+            <FontAwesomeIcon icon={faUser} className="me-2" />
+            Register
+          </NavLink>
+        </>
+      ) : (
+        <NavLink
+          to="/tuiter/profile"
+          className="list-group-item list-group-item-action"
+          activeClassName="active" 
+        >
+          <FontAwesomeIcon icon={faUser} className="me-2" />
+          Profile
+        </NavLink>
+      )}
+    </div>
+  );
+};
+
+export default NavigationSidebar;
